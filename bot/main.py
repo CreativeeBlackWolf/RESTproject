490c8ab0b7dd4ec6d37f494e7fae894487f39ac2
saqriphnix <creativeeblackwolf@gmail.com>
2022-04-26 01:51:59 +0300

    while true loop on start

--- bot/main.py
+++ bot/main.py
@@ -3,10 +3,10 @@ from bot.handlers.handler_config import bot
 from bot.handlers import commands_handler, events_handler
 from bot.api.api_requests import UserAPIRequest
 from bot.utils.redis_utils import add_new_users
+from requests.exceptions import ConnectionError
 from time import sleep
 
 
-# sleep(3) # wait until django will be loaded (kappa)
 app = FastAPI()
 wallets_api = UserAPIRequest()
 confirmation_code: str
@@ -16,7 +16,13 @@ secret: str
 @app.on_event("startup")
 async def startup():
     global confirmation_code, secret
-    users, status = wallets_api.get_users()
+    while True:
+        try:
+            users, status = wallets_api.get_users()
+            break
+        except ConnectionError:
+            print("cannot get to API server. retry...")
+            sleep(2)
     if status == 200:
         for u in users:
             add_new_users(u["id"])
